c14a44bad3762e43898a1aa7e51f305adfc6c9d4
2D\joshua.kim <joshua.kim@tigerspike.com>
2019-05-01 23:25:58 -0700

    another yarn build

--- src/Configure.tsx
+++ src/Configure.tsx
@@ -382,7 +382,7 @@ class Configure extends React.Component<any, State> {
             ws_list: [],
         });
         this.populateParamList();
-        alert("cleared");
+        alert('cleared');
     }
 
     // Once we have mounted, we call to initialize
